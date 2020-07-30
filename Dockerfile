9e10898ee2cbe35c6d3be203405a1e994397e2cc
Md. Mokammel Hossen Farnan <Farnan130117@users.noreply.github.com>
2020-07-31 00:58:01 +0600

    Add files via upload

---
+++ Dockerfile
@@ -0,0 +1,3 @@
+FROM php:7.4.1-apache
+COPY src/ /var/www/html/
+EXPOSE 80
---
+++ src/index.php
@@ -0,0 +1,9 @@
+<h1>
+<?php
+  //  /src/index.php
+  
+  //echo "Hello DOCKER";
+  echo "PHP_Version :".phpversion();
+  
+?>
+</h1>
